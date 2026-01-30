8422f95ea0de70476a43e13822d8c79dddc9397e
IvanKuleshin <ivan.kuleshin@tvh.com>
2026-01-30 15:27:24 +0100

    Fix version for Gatling

--- src/test/java/Engine.java
+++ src/test/java/Engine.java
@@ -1,14 +1,12 @@
 import io.gatling.app.Gatling;
-import io.gatling.core.config.GatlingPropertiesBuilder;
 
 public class Engine {
 
-  public static void main(String[] args) {
-    GatlingPropertiesBuilder props = new GatlingPropertiesBuilder()
-      .resourcesDirectory(IDEPathHelper.mavenResourcesDirectory.toString())
-      .resultsDirectory(IDEPathHelper.resultsDirectory.toString())
-      .binariesDirectory(IDEPathHelper.mavenBinariesDirectory.toString());
-
-    Gatling.fromMap(props.build());
-  }
+    public static void main(String[] args) {
+        String[] gatlingArgs = {
+                "--simulation", "acetoys.AceToysSimulation",
+                "--results-folder", IDEPathHelper.resultsDirectory.toString()
+        };
+        Gatling.main(gatlingArgs);
+    }
 }
