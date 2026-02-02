eab251c2eda7725b7edfaabddc089949bd24e375
IvanKuleshin <ivan.kuleshin@tvh.com>
2026-02-02 18:35:23 +0100

    Fix for Gatling 3.11 and above

--- src/test/java/acetoys/AceToysSimulation.java
+++ src/test/java/acetoys/AceToysSimulation.java
@@ -1,60 +1,68 @@
 package acetoys;
 
-import acetoys.pageobjects.*;
-import io.gatling.javaapi.core.*;
-import io.gatling.javaapi.http.*;
+import acetoys.pageobjects.Cart;
+import acetoys.pageobjects.Category;
+import acetoys.pageobjects.Customer;
+import acetoys.pageobjects.Product;
+import acetoys.pageobjects.StaticPages;
+import io.gatling.javaapi.core.ScenarioBuilder;
+import io.gatling.javaapi.core.Simulation;
+import io.gatling.javaapi.http.HttpProtocolBuilder;
 
-import static io.gatling.javaapi.core.CoreDsl.*;
-import static io.gatling.javaapi.http.HttpDsl.*;
+import static io.gatling.javaapi.core.CoreDsl.AllowList;
+import static io.gatling.javaapi.core.CoreDsl.DenyList;
+import static io.gatling.javaapi.core.CoreDsl.atOnceUsers;
+import static io.gatling.javaapi.core.CoreDsl.scenario;
+import static io.gatling.javaapi.http.HttpDsl.http;
 
 public class AceToysSimulation extends Simulation {
 
-  private static final String DOMAIN = "acetoys.uk";
+    private static final String DOMAIN = "acetoys.uk";
 
-  private HttpProtocolBuilder httpProtocol = http
-    .baseUrl("https://" + DOMAIN)
-    .inferHtmlResources(AllowList(), DenyList(".*\\.js", ".*\\.css", ".*\\.gif", ".*\\.jpeg", ".*\\.jpg", ".*\\.ico", ".*\\.woff", ".*\\.woff2", ".*\\.(t|o)tf", ".*\\.png", ".*detectportal\\.firefox\\.com.*"))
-    .acceptEncodingHeader("gzip, deflate")
-    .acceptLanguageHeader("en-GB,en;q=0.9");
+    private HttpProtocolBuilder httpProtocol = http
+            .baseUrl("https://" + DOMAIN)
+            .inferHtmlResources(AllowList(), DenyList(".*\\.js", ".*\\.css", ".*\\.gif", ".*\\.jpeg", ".*\\.jpg", ".*\\.ico", ".*\\.woff", ".*\\.woff2", ".*\\.(t|o)tf", ".*\\.png", ".*detectportal\\.firefox\\.com.*"))
+            .acceptEncodingHeader("gzip, deflate")
+            .acceptLanguageHeader("en-GB,en;q=0.9");
 
-  private ScenarioBuilder scn = scenario("AceToysSimulation")
-    .exec(StaticPages.homepage)
-    .pause(2)
-    .exec(StaticPages.ourStory)
-    .pause(2)
-    .exec(StaticPages.getInTouch)
-    .pause(2)
-    .exec(Category.productListByCategory_AllProducts)
-    .pause(2)
-    .exec(Category.loadSecondPageOfProducts)
-    .pause(2)
-    .exec(Category.loadThirdPageOfProducts)
-    .pause(2)
-    .exec(Product.loadProductDetailsPage_DartBoards)
-    .pause(2)
-    .exec(Product.addProductToCart_Product19)
-    .pause(2)
-    .exec(Category.productListByCategory_BabiesToys)
-    .pause(2)
-    .exec(Product.addProductToCart_Product4)
-    .pause(2)
-    .exec(Product.addProductToCart_Product5)
-    .pause(2)
-    .exec(Cart.viewCart)
-    .pause(2)
-    .exec(Customer.login)
-    .pause(2)
-    .exec(Cart.increaseQuantityInCart)
-    .pause(2)
-    .exec(Cart.increaseQuantityInCart)
-    .pause(2)
-    .exec(Cart.decreaseQuantityInCart)
-    .pause(2)
-    .exec(Cart.checkout)
-    .pause(2)
-    .exec(Customer.logout);
+    private ScenarioBuilder scn = scenario("AceToysSimulation")
+            .exec(StaticPages.homepage)
+            .pause(2)
+            .exec(StaticPages.ourStory)
+            .pause(2)
+            .exec(StaticPages.getInTouch)
+            .pause(2)
+            .exec(Category.productListByCategory_AllProducts)
+            .pause(2)
+            .exec(Category.loadSecondPageOfProducts)
+            .pause(2)
+            .exec(Category.loadThirdPageOfProducts)
+            .pause(2)
+            .exec(Product.loadProductDetailsPage_DartBoards)
+            .pause(2)
+            .exec(Product.addProductToCart_Product19)
+            .pause(2)
+            .exec(Category.productListByCategory_BabiesToys)
+            .pause(2)
+            .exec(Product.addProductToCart_Product4)
+            .pause(2)
+            .exec(Product.addProductToCart_Product5)
+            .pause(2)
+            .exec(Cart.viewCart)
+            .pause(2)
+            .exec(Customer.login)
+            .pause(2)
+            .exec(Cart.increaseQuantityInCart)
+            .pause(2)
+            .exec(Cart.increaseQuantityInCart)
+            .pause(2)
+            .exec(Cart.decreaseQuantityInCart)
+            .pause(2)
+            .exec(Cart.checkout)
+            .pause(2)
+            .exec(Customer.logout);
 
-  {
-	  setUp(scn.injectOpen(atOnceUsers(1))).protocols(httpProtocol);
-  }
+    {
+        setUp(scn.injectOpen(atOnceUsers(1))).protocols(httpProtocol);
+    }
 }
