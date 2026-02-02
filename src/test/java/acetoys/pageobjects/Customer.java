d7d60f24356e26b265a7dee15e7d9d2e32c2316c
IvanKuleshin <ivan.kuleshin@tvh.com>
2026-02-02 18:35:23 +0100

    Fix for Gatling 3.11 and above

--- src/test/java/acetoys/pageobjects/Customer.java
+++ src/test/java/acetoys/pageobjects/Customer.java
@@ -1,29 +1,34 @@
 package acetoys.pageobjects;
 
 import io.gatling.javaapi.core.ChainBuilder;
-import io.gatling.javaapi.core.Choice;
 
-import static io.gatling.javaapi.core.CoreDsl.*;
-import static io.gatling.javaapi.http.HttpDsl.*;
+import static io.gatling.javaapi.core.CoreDsl.css;
+import static io.gatling.javaapi.core.CoreDsl.exec;
+import static io.gatling.javaapi.core.CoreDsl.pause;
+import static io.gatling.javaapi.core.CoreDsl.percent;
+import static io.gatling.javaapi.core.CoreDsl.randomSwitch;
+import static io.gatling.javaapi.http.HttpDsl.http;
 
 public class Customer {
 
     public static ChainBuilder login =
-        exec(
-            http("Login User")
-                    .post("/login")
-                    .formParam("_csrf", "#{csrfToken}")
-                    .formParam("username", "user1")
-                    .formParam("password", "pass")
-                    .check(css("#_csrf", "content").saveAs("csrfTokenLoggedIn"))
-    );
+            exec(
+                    http("Login User")
+                            .post("/login")
+                            .formParam("_csrf", "#{csrfToken}")
+                            .formParam("username", "user1")
+                            .formParam("password", "pass")
+                            .check(css("#_csrf", "content").saveAs("csrfTokenLoggedIn"))
+            );
 
     public static ChainBuilder logout =
             randomSwitch().on(
-                    Choice.withWeight(10, exec(
+                    percent(10).then(exec(
                             http("Logout")
                                     .post("/logout")
                                     .formParam("_csrf", "#{csrfTokenLoggedIn}")
                                     .check(css("#LoginLink").is("Login"))
-                    )));
+                    )),
+                    percent(90).then(exec(pause(0)))
+            );
 }
